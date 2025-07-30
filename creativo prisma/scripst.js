8e75687d017232e1c87295623f19b4a2d72ba7ae
alexis992aa <alexisdiaz3744@gmail.com>
2025-07-30 08:51:12 -0400

    Add files via upload

---
+++ creativo prisma/scripst.js
@@ -0,0 +1,10 @@
+// Scroll suave a secciones
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
